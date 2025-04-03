81ac891807fc9bfc6cbaf5187707e2eeefbd2dce
adtyn-0 <adityan.nampoothiry1345@gmail.com>
2025-04-04 00:59:04 +0530

    first changes in a while

---
+++ pomodoro.py
@@ -0,0 +1,13 @@
+# imports
+import customtkinter as ctk
+import settings
+import blocker
+import timer_logic
+
+# Main Window
+app = ctk.CTk()
+app.title("Pomodoro Timer")
+app.geometry("400x300")
+
+
+app.mainloop()
